cd95d0ffea432cd945823cbc8ad07290bfbfcc46
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2015-05-28 18:44:41 +0200

    Add hat

--- sample/HelloWorld/templates/helloworld.php
+++ sample/HelloWorld/templates/helloworld.php
@@ -9,12 +9,22 @@
         var sessionId = '<?php echo $sessionId; ?>';
         var token = '<?php echo $token; ?>';
     </script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
     <script src="/js/helloworld.js"></script>
     <script src="/js/html2canvas.js"></script>
     <link rel="stylesheet" type="text/css" href="/css/game.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 </head>
 <body>
-    <h2>Hello, World!</h2>
+
     <script>
     var save = function() {
         html2canvas(document.body, {
@@ -24,9 +34,35 @@
         });
     }
     </script>
-    <div onclick="save()">Save</div>
-    <div id="publisher"></div>
-
-    <div id="subscribers"></div>
+    <header class="navbar navbar-static-top bs-docs-nav" id="navbar-header" role="banner" style="margin-bottom:60px;">
+        <div class="container">
+        <div class="navbar-header" id="header-web-logo">
+            <img src="https://static.opentok.com/img/img_www_nav_TBLogoReg.png">
+        </div>
+        <nav class="collapse navbar-collapse bs-navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active">
+              <a id="hideHat" href="#" onclick="$('#hideHat').hide();$('#hat').hide();$('#showHat').show();" style="display:none;">Hide hat</a>
+              <a id="showHat" href="#" onclick="$('#showHat').hide();$('#hat').show();$('#hideHat').show();">Show hat</a>
+            </li>
+            <li>
+              <a id="hide" href="#" onclick="$('#hide').hide();$('#mask').hide();$('#show').show();">Hide mask</a>
+              <a id="show" href="#" onclick="$('#show').hide();$('#mask').show();$('#hide').show();" style="display:none;">Show mask</a>
+            </li>
+          </ul>
+          <div class="navbar-right">
+             <span>Mix faces game :)</span>
+          </div>
+        </nav>
+    </header>
+    
+        <div id="mask"><img src="/images/face-02-02.png" /></div>
+        <div id="hat" style="display:none;"><img src="/images/giants_hat.png" /></div>
+        <div></div>
+        <div class="container">
+            <div id="publisher"></div>
+            <div id="subscribers"></div>
+        </div>
+    </div>
 </body>
 </html>
